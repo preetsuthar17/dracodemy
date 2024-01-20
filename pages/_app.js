53f0b0e79f46ccce5feba6d234a9d678747d3805
Preet Suthar <preetsutharxd@gmail.com>
2024-01-20 21:25:47 +0530

    refactor

--- pages/_app.js
+++ pages/_app.js
@@ -22,20 +22,7 @@ export default function App({ Component, pageProps }) {
       transition={{ duration: 1.3, transition: "ease-in" }}
     >
       <Layout>
-        <>
-          <script
-            data-name="BMC-Widget"
-            data-cfasync="false"
-            src="https://cdnjs.buymeacoffee.com/1.0.0/widget.prod.min.js"
-            data-id="preetsuthar"
-            data-description="Support me on Buy me a coffee!"
-            data-message=""
-            data-color="#FF813F"
-            data-position="Right"
-            data-x_margin="18"
-            data-y_margin="18"
-          ></script>
-        </>
+        <></>
         <Component {...pageProps} />
       </Layout>
     </motion.div>
