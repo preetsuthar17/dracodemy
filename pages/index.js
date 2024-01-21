179453e60856b79bbe1f1e6a221d2d69200e8960
Preet Suthar <preetsutharxd@gmail.com>
2024-01-21 12:50:31 +0530

    chore: update main button

--- pages/index.js
+++ pages/index.js
@@ -225,8 +225,8 @@ export default function Home({ posts, tags }) {
                         >
                           <svg
                             xmlns="http://www.w3.org/2000/svg"
-                            width="18"
-                            height="18"
+                            width="17"
+                            height="17"
                             viewBox="0 0 24 24"
                           >
                             <path
@@ -234,7 +234,7 @@ export default function Home({ posts, tags }) {
                               d="M5 21q-.825 0-1.412-.587T3 19V5q0-.825.588-1.412T5 3h7v2H5v14h14v-7h2v7q0 .825-.587 1.413T19 21zm4.7-5.3l-1.4-1.4L17.6 5H14V3h7v7h-2V6.4z"
                             />
                           </svg>
-                          Read
+                          Read More
                         </Link>
                       </div>
                     </div>
@@ -255,7 +255,12 @@ export default function Home({ posts, tags }) {
             >
               {visiblePosts.length < searchResults.length && (
                 <button onClick={handleLazyLoad} className=" primary-btn-main">
-                  Load More posts
+                  <span>
+                    <em>Load More posts</em>
+                  </span>
+                  <span>
+                    <em>Load More posts</em>
+                  </span>
                 </button>
               )}
             </div>
